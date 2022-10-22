90ab5284f4de5f3d6903360502fd4803f4b661c0
Yuki0528 <116093694+Yuki0528@users.noreply.github.com>
2022-10-23 08:55:20 +0900

    Add files via upload
    関数、変数まで完了

--- index.js
+++ index.js
@@ -1,52 +1,24 @@
-let wecando = 'iphone14pro';
+//変数
+let nakayou = 'stay hungry stay foolish';
 
-const wecando = 'we cannot connecting dots looking forward';
+//定数
+const wecando = 'iphone14pro';
 
-console.log(wecando)
+//配列
+const inoki = ['いち','に','さん'];
 
-<!doctype html>
-<html class="no-js" lang="">
+//ループ文
+//let index = 0;
+//while(index < inoki.length){　//=3
+    //繰り返したい命令
+//   console.log(inoki[index]);
+//index++; 
+//}
 
-<head>
-  <meta charset="utf-8">
-  <title></title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+//ifelse文
+if(inoki.length > 2){
+console.log('ボンバイエ！');
+}  else{
+    console.log('ボンバ..')
+}
 
-  <meta property="og:title" content="">
-  <meta property="og:type" content="">
-  <meta property="og:url" content="">
-  <meta property="og:image" content="">
-
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="apple-touch-icon" href="icon.png">
-  <!-- Place favicon.ico in the root directory -->
-<!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
-  <meta name="theme-color" content="#fafafa">
-</head>
-
-<body>
-
-<div class="container">
-Stay hungry Stay foolish
-
-<div class="mt-1 alert alert-primary" role="alert">
- ここに文字を入れてみたゼィ！!
-</div>
-
-<button type="button" class="ms-1 btn btn-primary">とりあえず青</button>
-<button type="button" class="ms-1 btn btn-secondary">Secondary</button>
-<button type="button" class="ms-2 btn btn-success">Success</button>
-<button type="button" class="ms-3 btn btn-danger">Danger</button>
-<button type="button" class="ms-1 btn btn-warning">Warning</button>
-
-<button type="button" class="btn btn-link">https://www.instagram.com/nakayou_alpaca/</button>
-
-
-</div>
-
-</body>
-
-</html>
