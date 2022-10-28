fd04149d7106c1e98749f1fa71e40d4f4ba34697
Yuki0528 <116093694+Yuki0528@users.noreply.github.com>
2022-10-28 21:19:07 +0900

    練習10月28日

--- app.js
+++ app.js
@@ -5,47 +5,35 @@ const answers = [
   'ニンテンドーDS',
   'ニンテンドースイッチ'
 ];
-
 const correct ='ニンテンドーDS';
 
-document.getElementById('js-question').textContent = question
-
 const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
 
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
+//クイズの問題文、選択肢定義
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = question
+  let buttonIndex = 0;
+while(buttonIndex < buttonLength){
+  $button[buttonIndex].textContent = answers[buttonIndex];
+  buttonIndex++;
+}
+}
+setupQuiz();
+
+const clickHandler = (e) => { if(correct === e.target.textContent){
+  window.alert('正解!');
+   } else {
+    window.alert('不正解!');
+   }
+  };
 
 //正誤判定
-$button[0].addEventListener('click', () => {
- if(correct === document.getElementsByTagName('button')[0].textContent){
-window.alert('正解!');
- } else {
-  window.alert('不正解!');
- }
-});
+let handlerIndex = 0;
+while (handlerIndex < buttonLength) {
+  $button[handlerIndex].addEventListener('click', (e) => {
+    clickHandler(e);
+  });
+  handlerIndex++;
+}
 
-$button[1].addEventListener('click', () => {
-  if(correct === document.getElementsByTagName('button')[1].textContent){
- window.alert('正解!');
-  } else {
-   window.alert('不正解!');
-  }
- });
-
- $button[2].addEventListener('click', () => {
-  if(correct === document.getElementsByTagName('button')[2].textContent){
- window.alert('正解!');
-  } else {
-   window.alert('不正解!');
-  }
- });
-
- $button[3].addEventListener('click', () => {
-  if(correct === document.getElementsByTagName('button')[3].textContent){
- window.alert('正解!');
-  } else {
-   window.alert('不正解!');
-  }
- });
