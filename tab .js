a2a21e42a830101ae265b332ef812254abf7ccdc
Yuki0528 <116093694+Yuki0528@users.noreply.github.com>
2022-11-11 22:48:15 +0900

    練習11月11日

--- tab .js
+++ tab .js
@@ -1,6 +1,36 @@
 (()=>{
 
-  //ここに命令を書く
+  const $doc = document;
+  const $tab = $doc.getElementById('js-tab');
+  const $nav = $tab.querySelectorAll('[data-nav]');
+  const $content = $tab.querySelectorAll('[data-content]');
 
-  
+  //初期化
+  const init = () => {
+    $content[0].getElementsByClassName.display = 'block';
+  };
+init();
+
+//クリックしたら怒るイベント
+const handleClick = (e) => {
+  e.preventDefault();
+
+  //クリックされたnavとそのdataを取得
+const $this = e.target;
+const targetVal = $this.dataset.nav;
+
+console.log('targetVal', targetVal);
+
+//対象のコンテンツをアクティブ化
+
+};
+
+//全nav要素に対して関数を適用
+let index = 0;
+while(index < $nav.length){
+  $nav[index].addEventListener('click', (e) => handleClick(e));
+  index++;
+}
+
+
 })();
