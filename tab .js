7a81d476367007daabd91849223f2bf5dccae2f5
Yuki0528 <116093694+Yuki0528@users.noreply.github.com>
2022-11-14 10:32:13 +0900

    練習11月14日　完成
    https://www.youtube.com/watch?v=OSQ1LnU9SCw&t=977s&ab_channel=%E3%82%BB%E3%82%A4%E3%83%88%E5%85%88%E7%94%9F%E3%81%AEWeb%E3%83%BBIT%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E8%BB%A2%E8%81%B7%E3%83%A9%E3%83%9C

--- tab .js
+++ tab .js
@@ -1,9 +1,13 @@
+//UIタブ実装練習
+
 (()=>{
 
   const $doc = document;
   const $tab = $doc.getElementById('js-tab');
   const $nav = $tab.querySelectorAll('[data-nav]');
   const $content = $tab.querySelectorAll('[data-content]');
+  const ACTIVE_CLASS = 'is-active';
+  const navLen = $nav.length
 
   //初期化
   const init = () => {
@@ -11,7 +15,7 @@
   };
 init();
 
-//クリックしたら怒るイベント
+//クリックしたら起こるイベント
 const handleClick = (e) => {
   e.preventDefault();
 
@@ -19,15 +23,25 @@ const handleClick = (e) => {
 const $this = e.target;
 const targetVal = $this.dataset.nav;
 
-console.log('targetVal', targetVal);
+//対象外のnav, content全て一旦リセットする
+let index = 0;
+while(index < navLen){
+$content[index].style.display = 'none';
+$nav[index].classList.remove('ACTIVE_CLASS');
+index++;
+}
+
 
 //対象のコンテンツをアクティブ化
+$tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
+$nav[targetVal].classList.add('ACTIVE_CLASS');
+
 
 };
 
 //全nav要素に対して関数を適用
 let index = 0;
-while(index < $nav.length){
+while(index < navLen){
   $nav[index].addEventListener('click', (e) => handleClick(e));
   index++;
 }
